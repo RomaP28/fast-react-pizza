ff4b99fab0322e1a1b18faf086fe121b6cd58116
RomaP28 <posokhovrom@gmail.com>
2024-11-14 00:17:58 +0200

    menu loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, {loader as menuLoader} from "./features/menu/Menu";
 import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
 
 const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/menu',  element: <Menu /> },
-  { path: '/cart', element: <Cart /> },
-  { path: '/order/new', element: <CreateOrder /> },
-  { path: '/order/:orderId', element: <Order /> }
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/menu',
+        element: <Menu />,
+        loader: menuLoader
+      },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> }
+    ],
+  },
 ])
 function App() {
   return <RouterProvider router={router} />;
