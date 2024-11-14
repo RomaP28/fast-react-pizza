ff44551ac818b29a33d6a9df0f757b668d51527b
RomaP28 <posokhovrom@gmail.com>
2024-11-14 09:30:25 +0200

    Error handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Cart from "./features/cart/Cart.jsx";
 import Order from "./features/order/Order.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       {
         path: '/menu',
         element: <Menu />,
-        loader: menuLoader
+        loader: menuLoader, // This will be called and fetched data when the route is navigated to
+        errorElement: <Error />,
       },
       { path: '/cart', element: <Cart /> },
       { path: '/order/new', element: <CreateOrder /> },
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -6,6 +6,7 @@ function Menu() {
   console.log(menu)
   return <ul>{menu.map(pizza => <MenuItem key={pizza.id} pizza={pizza} />)}</ul>;
 }
+
 export async function loader() {
   const menu = await getMenu();
   return menu;
